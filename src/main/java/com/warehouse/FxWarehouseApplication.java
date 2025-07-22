a933b425e94d1c53e96f087c9c9ebd6c2f169783
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-07-22 15:10:32 -0700

    initial commit

---
+++ src/main/java/com/warehouse/FxWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxWarehouseApplication.class, args);
+	}
+
+}
